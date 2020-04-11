45e794d260da0e9b91771ef5498eba654856d899
John Mai <jprebornzer@gmail.com>
2020-04-11 18:23:00 -0500

    Started Login

---
+++ zoodb/LoginPage.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace zoodb
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["zoodb"].ConnectionString);
+            connection.Open();
+            string checkUser = "select count(*) from db_user where email='" + TextBoxEmail.Text + "'";
+            MySqlCommand compare = new MySqlCommand(checkUser, connection);
+            int temp = Convert.ToInt32(compare.ExecuteScalar().ToString());
+            if (temp == 1)
+            {
+                Response.Write("User already Exists");
+            }
+            connection.Close();
+        }
+    }
+}
--- zoodb/Registration.aspx.cs
+++ zoodb/Registration.aspx.cs
@@ -16,17 +16,32 @@ namespace zoodb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(IsPostBack)
+            {
+                MySqlConnection connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["zoodb"].ConnectionString);
+                connection.Open();
+                string checkUser = "select count(*) from db_user where email='" + TextBoxEmail.Text + "'";
+                MySqlCommand compare = new MySqlCommand(checkUser, connection);
+                int temp = Convert.ToInt32(compare.ExecuteScalar().ToString());
+                if (temp == 1)
+                {
+                    Response.Write("User already Exists");
+                }
+                connection.Close();
+            }
         }
 
         protected void Registration_Click(object sender, EventArgs e)
         {
             try
             {
+                Guid newGuid = Guid.NewGuid();
+
                 MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["zoodb"].ConnectionString);
                 con.Open();
-                string insertUser = "insert into db_user (email, password) values (@Email, @Password)";
+                string insertUser = "insert into db_user (user_id, email, password) values (@User_ID, @Email, @Password)";
                 MySqlCommand cmd = new MySqlCommand(insertUser, con);
+                cmd.Parameters.AddWithValue("@User_ID", newGuid.ToString());
                 cmd.Parameters.AddWithValue("@Email", TextBoxEmail.Text);
                 cmd.Parameters.AddWithValue("@password", TextBox2PW.Text);
 
