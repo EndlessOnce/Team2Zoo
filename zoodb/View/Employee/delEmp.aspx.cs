d55dab3642f08a3a17e2a15a0ce7912d0d6e6519
Kenny <kennethz180@yahoo.com>
2020-04-13 19:13:12 -0500

    Uploaded Delete Pages
    Finished the delete pages

---
+++ zoodb/View/Employee/delEmp.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace zoodb.View.Employee
+{
+    public partial class delEmp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
