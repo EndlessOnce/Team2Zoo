4dd99cbbce81863f771ecc574e48ffc805f950a8
rkfung88 <61769858+rkfung88@users.noreply.github.com>
2020-04-14 22:31:52 -0500

    Add files via upload

--- zoodb/View/Animal/updateAnimal.aspx.cs
+++ zoodb/View/Animal/updateAnimal.aspx.cs
@@ -4,23 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
 namespace zoodb
 {
     public partial class updateAnimal : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+     
+        protected void updatePro_Click(object sender, EventArgs e)
         {
+            string animalName = String.Format("{0}", Request.Form["anName"]);
+            string health = String.Format("{0}", Request.Form["SelectH"]);
+            string exhibit = String.Format("{0}", Request.Form["Exhibit"]);
+            string diet = String.Format("{0}", Request.Form["diet1"]);
 
+
+            MySqlConnection link = new MySqlConnection(ConfigurationManager.ConnectionStrings["zoodb"].ConnectionString);
+            link.Open();
+            string updateQuery = "update animal SET Health=@health , Exhibit_ID=@exID , Diet=@diet WHERE Name=@name";
+            MySqlCommand comm = new MySqlCommand(updateQuery, link);
+            comm.Parameters.AddWithValue("@name", animalName);
+            comm.Parameters.AddWithValue("@health", health);
+            comm.Parameters.AddWithValue("@exID", exhibit);
+            comm.Parameters.AddWithValue("@Diet", diet);
+
+            comm.ExecuteNonQuery();
+            //labelAnimUp.Visible = true;
+            link.Close();
         }
-    }
-    public class MyViewModel
-    {
-        public string animalName { get; set; }
-        public string health { get; set; }
-        public string diet { get; set; }
-        public string exhibit { get; set; }
 
     }
 }
-//Health, Diet, Exhibit_ID
