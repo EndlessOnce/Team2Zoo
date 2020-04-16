977f52765acb402f8c924d63c46ae52207164431
John Mai <jprebornzer@gmail.com>
2020-04-16 01:57:29 -0500

    Finished Show Search Page

--- zoodb/View/Show/ShowSearch.aspx
+++ zoodb/View/Show/ShowSearch.aspx
@@ -118,9 +118,10 @@
 
 
                     <p>
-                        <strong>Date of Show:</strong>
-                        <input type="text" placeholder="YYYY-MM-DD hh:mm:ss" name="datetime" runat="server" style="width: 200px" id="showTime" />
-                        <button type="submit" runat="server" id="date_show">Search</button>
+                        <span style="margin-left: 2em;">
+                            <strong>Date of Show:</strong>
+                            <input type="text" placeholder="YYYY-MM-DD hh:mm:ss" name="datetime" runat="server" style="width: 200px" id="datetime" />
+                            <asp:Button ID="DateofShow" runat="server" Text="Search" OnClick="DateofShow_Click" />
                     </p>
 
 
@@ -129,11 +130,25 @@
                         <span style="margin-left: 2em;">
                             <strong>Name:</strong>
                             <input type="text" placeholder="Enter Show Name" name="ss_name" runat="server" style="width: 200px" id="ss_name" />
-                        <button type="submit" runat="server" id="search_show">Search</button>
+                            <asp:Button ID="ShowName" runat="server" Text="Search" OnClick="ShowName_Click" />
+                        </span>
+                    </p>
+                    <p>
+                        <span style="margin-left: 2em;">
+                            <strong>Location:</strong>
+                            <input type="text" placeholder="Enter Location" name="location" runat="server" style="width: 200px" id="location" />
+                            <asp:Button ID="LocationButton" runat="server" Text="Search" OnClick="LocationButton_Click" />
                         </span>
                     </p>
 
                     <p>
+                    </p>
+
+                      <p>
+                        <center>
+                        <asp:GridView ID="GridView1" runat="server" ShowHeaderWhenEmpty="true" emptyDataText="No items found!">
+                        </asp:GridView>
+                        </center>
                     </p>
 
                     <table id="showTable" border="1" width="100%" hidden="hidden">
--- zoodb/View/Show/ShowSearch.aspx.cs
+++ zoodb/View/Show/ShowSearch.aspx.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
 namespace zoodb
 {
     public partial class ShowSearch : System.Web.UI.Page
@@ -12,6 +14,54 @@ namespace zoodb
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void DateofShow_Click(object sender, EventArgs e)
+        {
+            MySqlConnection link = new MySqlConnection(ConfigurationManager.ConnectionStrings["zoodb"].ConnectionString);
+            link.Open();
+            string date = String.Format("{0}", Request.Form["datetime"]);
+
+            string searchQuery = "select * from shows where date_time like @date_time";
+            MySqlCommand comm = new MySqlCommand(searchQuery, link);
+            comm.Parameters.AddWithValue("@date_time", $"%{date}%");
+            DataTable dt = new DataTable();
+            MySqlDataAdapter sda = new MySqlDataAdapter(comm);
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void ShowName_Click(object sender, EventArgs e)
+        {
+            MySqlConnection link = new MySqlConnection(ConfigurationManager.ConnectionStrings["zoodb"].ConnectionString);
+            link.Open();
+            string description = String.Format("{0}", Request.Form["ss_name"]);
+
+            string searchQuery = "select * from shows where description like @description";
+            MySqlCommand comm = new MySqlCommand(searchQuery, link);
+            comm.Parameters.AddWithValue("@description", $"%{description}%");
+            DataTable dt = new DataTable();
+            MySqlDataAdapter sda = new MySqlDataAdapter(comm);
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void LocationButton_Click(object sender, EventArgs e)
+        {
+            MySqlConnection link = new MySqlConnection(ConfigurationManager.ConnectionStrings["zoodb"].ConnectionString);
+            link.Open();
+            string location = String.Format("{0}", Request.Form["location"]);
+
+            string searchQuery = "select * from shows where location like @location";
+            MySqlCommand comm = new MySqlCommand(searchQuery, link);
+            comm.Parameters.AddWithValue("@location", $"%{location}%");
+            DataTable dt = new DataTable();
+            MySqlDataAdapter sda = new MySqlDataAdapter(comm);
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
         }
     }
 }
