c9cbb615c3986343cf4edb837b3e9a24284a2488
mortar&pestle <chasebowman13@gmail.com>
2020-04-16 16:48:21 -0500

    Changed searchProdcut

--- zoodb/View/Product/SearchProduct.aspx.cs
+++ zoodb/View/Product/SearchProduct.aspx.cs
@@ -49,5 +49,22 @@ namespace zoodb
             GridView1.DataSource = dt;
             GridView1.DataBind();
         }
+
+        protected void DDLPro_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MySqlConnection link = new MySqlConnection(ConfigurationManager.ConnectionStrings["zoodb"].ConnectionString);
+            link.Open();
+            string name = String.Format("{0}", Request.Form["DDLPro"]);
+
+            string searchQuery = "select * from product where name like @name";
+            MySqlCommand comm = new MySqlCommand(searchQuery, link);
+            comm.Parameters.AddWithValue("@name", $"%{name}%");
+            DataTable dt = new DataTable();
+            MySqlDataAdapter sda = new MySqlDataAdapter(comm);
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
     }
 }
